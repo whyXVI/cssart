7f88a9bae7e70de2613aab456d8b37c73c771750
L <the_Beatle122@outlook.com>
2025-06-18 16:02:04 +0800

    gh pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // custom gh pages base path
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
